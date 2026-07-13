a0d524c0d4d12218d43b228093562d6dbb9a3e32
JYOTHIk94 <jyothikprapoyil18@gmail.com>
2026-07-13 12:55:50 +0530

    Company fix

---
+++ property_management/property_management/doctype/property_settings/property_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Jyothi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Property Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
